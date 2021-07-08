873cb81acd5d91fab462ac71d8ffa3c8f3e752bb
Hansaem Kim <hansaem_kim@hotmail.com>
2021-07-08 16:43:04 -0400

    Finished Tower_Hanoi

--- lib/towers_of_hanoi.rb
+++ lib/towers_of_hanoi.rb
@@ -16,19 +16,21 @@ class HanoiTower
     end
 
     def valid_move?(start_pos, end_pos)
-        if @towers[end_pos].empty? || @towers[start_pos].last > @towers[end_pos].last
+        if @towers[end_pos].empty? || @towers[start_pos].last < @towers[end_pos].last
             return true
         end
+            
         false
     end
 
     def win?
         arr = []
         (1..size).each do |i|
-            arr[0].unshift(i)
+            arr.unshift(i)
         end
         if @towers[2] == arr
-            return true
+            print "You Won!!!"
+            return true 
         end
         false
     end
@@ -45,8 +47,42 @@ class HanoiTower
             raise "that's not a valid move!"
         end
     end
-    #play
-        #gets from player
-    #display
-    #move piece
+
+    def display
+        @towers.each_with_index do |tower, index|
+            puts "Tower #{index}: #{tower}"
+        end
+    end
+
+    def play
+        self.populate
+        until self.win?
+            begin
+                self.turn
+            rescue 
+                puts "try again"
+                sleep(1)
+                retry
+            end    
+        end
+    end
+
+    def turn
+        system("clear")
+        self.display
+
+        puts "Enter the tower that you want to move from: "
+        start_pos = gets.chomp.to_i
+
+        puts "Enter the tower that you want to move to: "
+        end_pos = gets.chomp.to_i
+
+        self.move_piece(start_pos, end_pos)
+
+    end
+
 end
+
+# game = HanoiTower.new(3)
+# game.play
+
--- spec/towers_of_hanoi_spec.rb
+++ spec/towers_of_hanoi_spec.rb
@@ -18,4 +18,64 @@ describe HanoiTower do
         end
     end
 
+    describe "#valid_move?" do
+        it "should accept start_pos and end_pos as arguments" do
+            expect {game.valid_move?(0,1)}.to_not raise_error
+        end
+        
+        it "return true if the tower at end_pos is empty" do
+            game.towers[0] = [3,2]
+            game.towers[1] = [1]
+            game.towers[2] = []
+            expect(game.valid_move?(0,2)).to eq(true)
+        end
+    end
+
+    describe "#win?" do
+        it "return true if the third tower is correctly filled" do
+            game.towers[2] = [3,2,1]
+            game.towers[0] = []
+            expect(game.win?).to eq(true)
+        end
+    end
+
+    describe "#move_piece" do
+        it "should accept start_pos and end_pos as arguments" do
+            expect {game.move_piece(0,1)}.to_not raise_error
+        end
+        it "should move pieces" do 
+            game.towers[0] = [3,2]
+            game.towers[1] = [1]
+            game.towers[2] = []
+            game.move_piece(0,2)
+            expect(game.towers[0]).to eq([3])
+            expect(game.towers[1]).to eq([1])
+            expect(game.towers[2]).to eq([2])
+        end
+        it "should raise an error if move is not valid" do
+            game.towers[0] = [3,2]
+            game.towers[1] = [1]
+            game.towers[2] = []
+            expect {game.move_piece(0,1)}.to raise_error
+        end
+    end
+
+    describe "#display" do
+        it "should display towers" do
+        end
+    end
+
+    describe "#turn" do
+        it "should get starting position from a player" do
+            input = double("0\n", :chomp=>"0")
+            allow(game.turn).to receive(:gets).and_return(input)
+
+            expect(input).to receive(:chomp)
+            expect(game.turn).to receive(:gets)
+        end
+
+    end
+
+
+
 end
